64a9b46bfa2caef58dcfd751b21768e35f6ffc33
Mario Castro Squella <mario.castro.squella@gmail.com>
2016-10-03 22:33:42 -0300

    Added another test for testing merge sort.

--- src/test/scala/week1/SortingSpec.scala
+++ src/test/scala/week1/SortingSpec.scala
@@ -17,4 +17,8 @@ class SortingSpec extends FlatSpec with Matchers {
   it should "return the sorted array" in {
     Sorting.mergeSort(Array(3, 5, 1, 4, 2)) shouldEqual Array(1, 2, 3, 4, 5)
   }
+
+  it should "return the sorted array when an array has repeated elements" in {
+    Sorting.mergeSort(Array(3, 5, 2, 4, 2)) shouldEqual Array(2, 2, 3, 4, 5)
+  }
 }
