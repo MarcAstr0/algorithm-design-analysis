287e25041b202c2525c0c3bd4d05cd7149eb35a7
Mario Castro Squella <mario.castro.squella@gmail.com>
2016-10-06 17:19:03 -0300

    Code and tests for Assignment #1.

---
+++ src/main/scala/week1/AssignmentOne.scala
@@ -0,0 +1,50 @@
+package week1
+
+/**
+ * Created by mcastro on 10/6/2016.
+ */
+object AssignmentOne {
+
+  def countInversions(arr: Array[Int], inversions: BigInt): (Array[Int], BigInt) = {
+    def merge(a: (Array[Int], BigInt), b: (Array[Int], BigInt)): (Array[Int], BigInt) = {
+      val n = a._1.length + b._1.length
+      val c = Array.fill[Int](n)(0)
+      var i, j = 0
+      var inv = a._2 + b._2
+      for (k <- 0 to n-1) {
+        if (i > a._1.length-1) {
+          c(k) = b._1(j)
+          j += 1
+        } else if (j > b._1.length-1) {
+          c(k) = a._1(i)
+          i += 1
+        } else {
+          if (a._1(i) <= b._1(j)) {
+            c(k) = a._1(i)
+            i += 1
+          } else if (a._1(i) > b._1(j)) {
+            c(k) = b._1(j)
+            j += 1
+            inv += (a._1.length - i)
+          }
+        }
+      }
+      (c, inv)
+    }
+
+    if (arr.length == 1) (arr, 0)
+    else {
+      val a = arr.take((arr.length/2.0).floor.toInt)
+      val b = arr.takeRight((arr.length/2.0).ceil.toInt)
+      merge(countInversions(a, inversions), countInversions(b, inversions))
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val stream = getClass.getResourceAsStream("/week1/IntegerArray.txt")
+    val lines = scala.io.Source.fromInputStream(stream).getLines
+    val inputArray = (for { line <- lines } yield line.toInt).toArray
+
+    println(countInversions(inputArray, 0)._2)
+  }
+}
---
+++ src/test/scala/week1/AssignmentOneSpec.scala
@@ -0,0 +1,19 @@
+package week1
+
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * Created by mcastro on 10/6/2016.
+ */
+class AssignmentOneSpec extends FlatSpec with Matchers {
+
+  "countInversions()" should "return 0 for a sorted array" in {
+    AssignmentOne.countInversions(Array(1, 2, 3, 4), 0)._2 shouldEqual 0
+  }
+
+  it should "count the correct number of inversions" in {
+    AssignmentOne.countInversions(Array(1, 3, 5, 2, 4, 6), 0)._2 shouldEqual 3
+    AssignmentOne.countInversions(Array(2, 4, 1, 3, 5), 0)._2 shouldEqual 3
+    AssignmentOne.countInversions(Array(6, 5, 4, 3, 2, 1), 0)._2 shouldEqual 15
+  }
+}
