123954fb461f0d140748ef6da84f9291d385b669
Mario Castro Squella <mario.castro.squella@gmail.com>
2016-10-11 18:01:35 -0300

    Implementation of Strassen's algorithm for Matrix multiplication. Doesn't perform as expected though...

--- src/main/scala/week1/MatrixMultiplication.scala
+++ src/main/scala/week1/MatrixMultiplication.scala
@@ -1,5 +1,7 @@
 package week1
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
  * Created by mcastro on 10/7/2016.
  */
@@ -15,6 +17,75 @@ object MatrixMultiplication {
       }
     }
     c
+  }
+
+  def strassen(x: Array[Array[Int]], y: Array[Array[Int]]): Array[Array[Int]] = {
+    if (x.length == 2 && y.length == 2) {
+      val p1 = x(0)(0) * (y(0)(1) - y(1)(1))
+      val p2 = (x(0)(0) + x(0)(1)) * y(1)(1)
+      val p3 = (x(1)(0) + x(1)(1)) * y(0)(0)
+      val p4 = x(1)(1) * (y(1)(0) - y(0)(0))
+      val p5 = (x(0)(0) + x(1)(1)) * (y(0)(0) + y(1)(1))
+      val p6 = (x(0)(1) - x(1)(1)) * (y(1)(0) + y(1)(1))
+      val p7 = (x(0)(0) - x(1)(0)) * (y(0)(0) + y(0)(1))
+      Array(Array(p5 + p4 - p2 + p6, p1 + p2), Array(p3 + p4, p1 + p5 - p3 - p7))
+    } else {
+      val topX = x.slice(0, x.length/2)
+      val bottomX = x.slice(x.length/2, x.length)
+      val topY = y.slice(0, y.length/2)
+      val bottomY = y.slice(y.length/2, y.length)
+      var a, b, c, d, e, f, g, h, r = ArrayBuffer[Array[Int]]()
+
+      // Sub-matrices for X
+      for {i <- 0 until topX.length} { a += topX(i).slice(0, topX(i).length/2) } // A
+      for {i <- 0 until topX.length} { b += topX(i).slice(topX(i).length/2, topX(i).length) } // B
+      for {i <- 0 until bottomX.length} { c += bottomX(i).slice(0, bottomX(i).length/2) } // C
+      for {i <- 0 until bottomX.length} { d += bottomX(i).slice(bottomX(i).length/2, bottomX(i).length) } // D
+      // Sub-matrices for Y
+      for {i <- 0 until topY.length} { e += topY(i).slice(0, topY(i).length/2) } // E
+      for {i <- 0 until topY.length} { f += topY(i).slice(topX(i).length/2, topY(i).length) } // F
+      for {i <- 0 until bottomY.length} { g += bottomY(i).slice(0, bottomY(i).length/2) } // G
+      for {i <- 0 until bottomY.length} { h += bottomY(i).slice(bottomY(i).length/2, bottomY(i).length) } // H
+
+      // The 7 products
+      val p1 = strassen(a.toArray, subtract(f.toArray, h.toArray))
+      val p2 = strassen(sum(a.toArray, b.toArray), h.toArray)
+      val p3 = strassen(sum(c.toArray, d.toArray), e.toArray)
+      val p4 = strassen(d.toArray, subtract(g.toArray, e.toArray))
+      val p5 = strassen(sum(a.toArray, d.toArray), sum(e.toArray, h.toArray))
+      val p6 = strassen(subtract(b.toArray, d.toArray), sum(g.toArray, h.toArray))
+      val p7 = strassen(subtract(a.toArray, c.toArray), sum(e.toArray, f.toArray))
+
+      val r1 = sum(subtract(sum(p5, p4), p2), p6)
+      val r2 = sum(p1, p2)
+      val r3 = sum(p3, p4)
+      val r4 = subtract(subtract(sum(p1, p5), p3), p7)
+
+      // we join in a single matrix
+      for {i <- 0 to r1.length-1} {r += r1(i) ++ r2(i) }
+      for {i <- 0 to r3.length-1} {r += r3(i) ++ r4(i) }
+      r.toArray
+    }
+  }
+
+  def sum(x: Array[Array[Int]], y: Array[Array[Int]]): Array[Array[Int]] = {
+    val result = Array.fill[Array[Int]](x.length)(Array.fill[Int](x(0).length)(0))
+    for {i <- 0 to x.length-1} {
+      for {j <- 0 to x.length-1} {
+        result(i)(j) = x(i)(j) + y(i)(j)
+      }
+    }
+    result
+  }
+
+  def subtract(x: Array[Array[Int]], y: Array[Array[Int]]): Array[Array[Int]] = {
+    val result = Array.fill[Array[Int]](x.length)(Array.fill[Int](x(0).length)(0))
+    for {i <- 0 to x.length-1} {
+      for {j <- 0 to x.length-1} {
+        result(i)(j) = x(i)(j) - y(i)(j)
+      }
+    }
+    result
   }
 
 }
--- src/test/scala/week1/MatrixMultiplicationSpec.scala
+++ src/test/scala/week1/MatrixMultiplicationSpec.scala
@@ -28,4 +28,53 @@ class MatrixMultiplicationSpec extends FlatSpec with Matchers {
     val i = Array(Array(1, 0, 0), Array(0, 1, 0), Array(0, 0, 1))
     MatrixMultiplication.multiply(a, i) shouldEqual a
   }
+
+  "strassen" should "yield the same results using Strassen algorithm that you get with the iterative method" in {
+    val a = Array(Array(1, 2), Array(3, 4))
+    val b = Array(Array(5, 6), Array(7, 8))
+    MatrixMultiplication.strassen(a, b) shouldEqual MatrixMultiplication.multiply(a, b)
+  }
+
+  it should "multiply 2 matrices of dimensions 4x4 return a matrix of 4x4" in {
+    val a = Array(Array(5, 2, 6, 1), Array(0, 6, 2, 0), Array(3, 8, 1, 4), Array(1, 8, 5, 6))
+    val b = Array(Array(7, 5, 8, 0), Array(1, 8, 2, 6), Array(9, 4, 3, 8), Array(5, 3, 7, 9))
+    MatrixMultiplication.strassen(a, b) shouldEqual Array(Array(96, 68, 69, 69), Array(24, 56, 18, 52), Array(58, 95, 71, 92), Array(90, 107, 81, 142))
+  }
+
+  it should "multiply 2 matrices of dimensions 8x8 return a matrix of 8x8" in {
+    val a = Array.fill[Array[Int]](8)(Array.fill[Int](8)(scala.util.Random.nextInt(10)))
+    val b = Array.fill[Array[Int]](8)(Array.fill[Int](8)(scala.util.Random.nextInt(10)))
+    MatrixMultiplication.strassen(a, b) shouldEqual MatrixMultiplication.multiply(a, b)
+  }
+
+  it should "confirm that the Strassen algorithm is faster" in {
+    def time[R](block: => R): R = {
+      val t0 = System.nanoTime()
+      val result = block    // call-by-name
+      val t1 = System.nanoTime()
+      println("Elapsed time: " + (t1 - t0) + "ns")
+      result
+    }
+
+    val n = 8
+    val a = Array.fill[Array[Int]](n)(Array.fill[Int](n)(scala.util.Random.nextInt(10)))
+    val b = Array.fill[Array[Int]](n)(Array.fill[Int](n)(scala.util.Random.nextInt(10)))
+
+    val r1 = time { MatrixMultiplication.multiply(a, b) }
+    val r2 = time { MatrixMultiplication.strassen(a, b) }
+
+    r1 shouldEqual r2
+  }
+
+  "sum" should "compute the sum of 2 arrays" in {
+    val a = Array(Array(1, 2), Array(3, 4))
+    val b = Array(Array(5, 6), Array(7, 8))
+    MatrixMultiplication.sum(a, b) shouldEqual Array(Array(6, 8), Array(10, 12))
+  }
+
+  "subtract" should "compute the subtraction of 2 arrays" in {
+    val a = Array(Array(1, 2), Array(3, 4))
+    val b = Array(Array(5, 6), Array(7, 8))
+    MatrixMultiplication.subtract(a, b) shouldEqual Array(Array(-4, -4), Array(-4, -4))
+  }
 }
